4164b91c8435b5c3a4b89570c1e79b02227975b3
Christopher <christopher@ChristophersMBP.attlocal.net>
2022-08-28 09:47:10 -0400

    Added avatar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './navbar.scss'
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
-import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
-
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined'
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined'
+import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined'
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined'
+import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
 export default function NavBar(){
     return (
         <>
@@ -21,11 +23,21 @@ export default function NavBar(){
                         </div>
                         <div className="item"> 
                             <DarkModeOutlinedIcon className="icon" />
-                            Dark Mode
                         </div>
                         <div className="item"> 
                             <ChatBubbleOutlineOutlinedIcon className="icon" />
-                            Chat
+                        </div>
+                        <div className="item"> 
+                            <FullscreenExitOutlinedIcon className="icon" />
+                        </div>
+                        <div className="item"> 
+                            <NotificationsOutlinedIcon  className="icon" />
+                        </div>
+                        <div className="item"> 
+                            <FormatListBulletedOutlinedIcon className="icon" />
+                        </div>
+                        <div className="item"> 
+                            <img src="https://images.unsplash.com/photo-1607746882042-944635dfe10e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80" alt="" className="avatar" />
                         </div>
                     </div>
                 </div>
